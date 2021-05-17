c8ba43c1b0ab71a7d2dbd14890c41a319a1f8559
yuto urushima <yuto259233@gmail.com>
2021-05-17 23:02:45 +0900

    変更

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,10 +1,5 @@
-import { useRouter } from 'next/router'
-
-const Post = () => {
-    const router = useRouter();
-    const { pid } = router.query;
-
-    return <p>Post: {pid}</p>
-};
-
-export default Post;
+export default ({post}) => {
+    return (
+        <div>Hello</div>
+    )
+}
