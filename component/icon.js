5a72e57ef5d5ae7e403407686ad94bbddc13b819
yuto urushima <yuto259233@gmail.com>
2021-05-20 22:44:36 +0900

    コンポーネントに分割 アセット類の追加 スタイル

---
+++ component/icon.js
@@ -0,0 +1,9 @@
+import styles from '../styles/components/icon.module.scss'
+
+export const ICON = () => {
+    return (
+        <div className={styles.icon}>
+            <img src='./b-portrait.png' alt='this is the icon'/>
+        </div>
+    )
+}
