477bf6d8d8619d1cb0d1d7b17616ea0ab54e4da3
yuto urushima <yuto259233@gmail.com>
2021-06-21 20:52:30 +0900

    100records 制限

--- component/graphql.js
+++ component/graphql.js
@@ -2,7 +2,7 @@ import { gql } from '@apollo/client'
 
 export const GET_ISSUES = gql`
     query getIssues {
-        search(query: "repo:YutoUrushima/github-issue-next-blog is:issue", type: ISSUE, first: 250) {
+        search(query: "repo:YutoUrushima/github-issue-next-blog is:issue", type: ISSUE, first: 100) {
             issueCount
             nodes {
                 ... on Issue {
@@ -13,7 +13,7 @@ export const GET_ISSUES = gql`
                     }
                     bodyHTML
                     createdAt
-                    labels(first: 250) {
+                    labels(first: 100) {
                         totalCount
                         nodes {
                             id
