d27508537b2437c14acb7fc965fd257bb9f43c0e
YutoUrushima <yuto259233@gmail.com>
2023-05-03 01:18:25 +0900

    Fix js to ts

--- component/graphql.ts
+++ component/graphql.ts
@@ -1,28 +1,28 @@
-import { gql } from '@apollo/client'
+import { gql } from "@apollo/client";
 
 export const GET_ISSUES = gql`
-    query getIssues {
-        search(query: "repo:YutoUrushima/github-issue-next-blog is:issue", type: ISSUE, first: 100) {
-            issueCount
+  query getIssues {
+    search(query: "repo:YutoUrushima/github-issue-next-blog is:issue", type: ISSUE, first: 100) {
+      issueCount
+      nodes {
+        ... on Issue {
+          number
+          title
+          author {
+            login
+          }
+          bodyHTML
+          createdAt
+          labels(first: 100) {
+            totalCount
             nodes {
-                ... on Issue {
-                    number
-                    title
-                    author {
-                        login
-                    }
-                    bodyHTML
-                    createdAt
-                    labels(first: 100) {
-                        totalCount
-                        nodes {
-                            id
-                            color
-                            name
-                        }
-                    }
-                }
+              id
+              color
+              name
             }
+          }
         }
+      }
     }
-`
+  }
+`;
