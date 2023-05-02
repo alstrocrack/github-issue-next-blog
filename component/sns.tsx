40b90d94f612e8a8399fed91e11cf53c9071e389
YutoUrushima <yuto259233@gmail.com>
2023-05-03 00:37:42 +0900

    Fix Link bugs

--- component/sns.tsx
+++ component/sns.tsx
@@ -1,27 +1,24 @@
-import styles from '../styles/components/sns.module.scss'
-import Link from 'next/link'
-import classNames from 'classnames'
+import styles from "../styles/components/sns.module.scss";
+import classNames from "classnames";
 
 const SNSList = [
-    {title: 'github', link: 'https://github.com/YutoUrushima'},
-    {title: 'twitter', link: 'https://twitter.com/Frontend_1220'},
-    {title: 'line', link: 'https://developers.line.biz/ja/'},
-]
+  { title: "github", link: "https://github.com/YutoUrushima" },
+  { title: "twitter", link: "https://twitter.com/Frontend_1220" },
+  { title: "line", link: "https://developers.line.biz/ja/" },
+];
 
 export const SNS = () => {
-    return (
-        <ul className={styles.sns}>
-            {SNSList.map((sns, index) => {
-                return (
-                    <li className={classNames(sns.title, styles.snsItem)} key={index}>
-                        <Link href={sns.link}>
-                            <a target="_blank" rel="noopener noreferrer">
-                                <img src={`./b-${sns.title}-icon.png`} alt={sns.title}/>
-                            </a>
-                        </Link>
-                    </li>
-                )
-            })}
-        </ul>
-    )
-}
+  return (
+    <ul className={styles.sns}>
+      {SNSList.map((sns, index) => {
+        return (
+          <li className={classNames(sns.title, styles.snsItem)} key={index}>
+            <a href={sns.link} target="_blank" rel="noopener noreferrer">
+              <img src={`./b-${sns.title}-icon.png`} alt={sns.title} />
+            </a>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
